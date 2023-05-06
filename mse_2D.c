425d3f7a9b116765e3a53fa59f1c6e3765c6f7ca
bcm <bcmardini@gmail.com>
2023-05-06 13:18:25 -0400

    mse_2D.1

---
+++ mse_2D.c
@@ -0,0 +1,195 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+double** read_csv(const char* filename) {
+    double** data = NULL;
+    FILE* file = fopen(filename, "r");
+    char line[1024];
+    int row_count = 0;
+    int col_count = 0;
+    
+    if (file == NULL) {
+        printf("Could not open file: %s\n", filename);
+        return NULL;
+    }
+    
+    // Count the number of rows and columns in the file
+    while (fgets(line, 1024, file)) {
+        col_count = 0;
+        char* token = strtok(line, ",");
+        while (token != NULL) {
+            col_count++;
+            token = strtok(NULL, ",");
+        }
+        row_count++;
+    }
+    
+    // Allocate memory for the data array
+    data = (double**)malloc(row_count * sizeof(double*));
+    for (int i = 0; i < row_count; i++) {
+        data[i] = (double*)malloc(col_count * sizeof(double));
+    }
+    
+    // Reset the file pointer and read the data into the array
+    fseek(file, 0, SEEK_SET);
+    int row = 0;
+    while (fgets(line, 1024, file)) {
+        int col = 0;
+        char* token = strtok(line, ",");
+        while (token != NULL) {
+            data[row][col] = atof(token);
+            col++;
+            token = strtok(NULL, ",");
+        }
+        row++;
+    }
+    
+    fclose(file);
+    return data;
+}
+
+double** coarse_graining(double** arr, int rows, int cols, int scale_factor) {
+    if (scale_factor == 1) {
+        return arr;
+    }
+    int new_rows = rows / scale_factor;
+    int new_cols = cols / scale_factor;
+    double** cg_arr = malloc(new_rows * sizeof(double*));
+    for (int i = 0; i < new_rows; i++) {
+        cg_arr[i] = malloc(new_cols * sizeof(double));
+        for (int j = 0; j < new_cols; j++) {
+            double sum = 0;
+            for (int x = i * scale_factor; x < (i + 1) * scale_factor; x++) {
+                for (int y = j * scale_factor; y < (j + 1) * scale_factor; y++) {
+                    sum += arr[x][y];
+                }
+            }
+            cg_arr[i][j] = sum / (scale_factor * scale_factor);
+        }
+    }
+    return cg_arr;
+}
+
+
+double max_distance(int m, double **image, int i, int j, int a, int b) {
+    double max_dist = 0;
+    for(int k=0; k<m; k++) {
+        for(int l=0; l<m; l++) {
+            double dist = fabs(image[i+k][j+l] - image[a+k][b+l]);
+            if(dist > max_dist) {
+                max_dist = dist;
+            }
+        }
+    }
+    return max_dist;
+}
+
+float calculate_U_ij_m(double **image, int i, int j, int m, double r, int H, int W) {
+    int count = 0;
+    int N_m = (H - m) * (W - m);
+    for (int a = 0; a < H - m; a++) {
+        for (int b = 0; b < W - m; b++) {
+            if (a == i && b == j) {
+                continue;
+            }
+            if (max_distance(m, image, i, j, a, b) <= r) {
+                count++;
+            }
+        }
+    }
+    // printf("Um %d: \n", count / (N_m-1));
+    return (float) count / (N_m-1);
+}
+
+float calculate_U_ij_m_plus_one(double **image, int i, int j, int m, double r, int H, int W) {
+    int count = 0;
+    int N_m = (H - m) * (W - m);
+    for (int a = 0; a < H - m; a++) {
+        for (int b = 0; b < W - m; b++) {
+            if (a == i && b == j) {
+                continue;
+            }
+            if (max_distance(m, image, i, j, a, b) <= r) {
+                count++;
+            }
+        }
+    }
+    return (float) count / (N_m-1);
+}
+
+
+float calculate_U_m(double **image, int m, double r, int H, int W) {
+    float sum = 0.0;
+    for (int i = 0; i < H - m; i++) {
+        for (int j = 0; j < W - m; j++) {
+            sum += calculate_U_ij_m(image, i, j, m, r, H, W);
+        }
+    }
+    // printf("Um %f: \n", sum / ((H - m) * (W - m)));
+    return sum / ((H - m) * (W - m));
+}
+
+
+float calculate_U_m_plus_one(double **image, int m, double r, int H, int W) {
+    m += 1;
+    float sum = 0.0;
+    for (int i = 0; i < H - m; i++) {
+        for (int j = 0; j < W - m; j++) {
+            sum += calculate_U_ij_m_plus_one(image, i, j, m, r, H, W);
+        }
+    }
+    // printf("Ump %f: \n", sum / ((H - m) * (W - m)));
+    return sum / ((H - m) * (W - m));
+}
+
+
+float negative_logarithm(float um, float umplus1) {
+    if (um == 0) {
+        return 0;
+    }
+    else {  
+        float result = -log(umplus1 / um);
+        return result;  
+    }
+}
+
+
+int main(int argc, char *argv[]) {
+    
+    char* csv_path = argv[1];
+    double** data = read_csv(csv_path);
+    int scales = atoi(argv[2]);
+    int rows = atoi(argv[3]);
+    int cols = atoi(argv[4]);
+    int m = atoi(argv[5]);
+    int r = atoi(argv[6]);
+    
+    double* n_values = malloc(scales * sizeof(double));
+
+    for (int i = 1; i <= scales; i++) {
+        double** coarse_data = coarse_graining(data, rows, cols, i);
+        float U_m = calculate_U_m(coarse_data, m, r, rows/i, cols/i);
+        float U_m_plus_one = calculate_U_m_plus_one(coarse_data, m, r, rows/i, cols/i);
+        float n = negative_logarithm(U_m, U_m_plus_one);
+        n_values[i-1] = n;
+    }
+
+    for (int i = 0; i < scales; i++) {
+        printf("%f ", n_values[i]);
+    }
+    printf("\n");
+
+    // free(n_values);
+    // free(data[0]);
+    // free(data);
+
+    return 0;
+}
+
+
+/*
+gcc mse_2D.c -o mse_2D -lm
+./mse_2D
+*/
---
+++ mse_2D_C.py
@@ -0,0 +1,88 @@
+import subprocess
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+import os
+from PIL import Image
+import time
+
+def plot_arrays(data_list, title='', xlabel='', ylabel='', legends=None, save_path=None):
+    fig, ax = plt.subplots()
+    plt.grid('black')
+    ax.set_facecolor('lightgrey')
+    for i, data_tuple in enumerate(data_list):
+        name, data = data_tuple
+        if legends is not None and len(legends) == len(data_list):
+            label = legends[i]
+        else:
+            label = name
+        plt.plot(data, color=f'C{i}', marker='v', markersize=5, label=label, markeredgecolor='black')
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend()
+    if save_path is not None:
+        plt.savefig(save_path)
+    plt.show()
+
+def calculate_std(csv_path):
+    unique_values = set()
+
+    with open(csv_path, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            for val in row:
+                unique_values.add(float(val))
+
+    std = np.std(list(unique_values))
+
+    return std
+
+def image_to_array(image_path):
+    img = Image.open(image_path).convert('L')
+    arr = np.array(img)
+    return arr
+
+def run_c_program(csv_path, scales, rows, cols, m, r):
+    command = ['./mse_2D', csv_path, str(scales), str(rows), str(cols), str(m), str(r)]
+    result = subprocess.run(command, stdout=subprocess.PIPE)
+    output = result.stdout.decode('utf-8').strip()
+    n_values = list(output.split())
+    n_values = [float(x) for x in n_values]
+    return n_values
+
+def mse_2D(folder_path, scales, m, r):
+    mse_values = []
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        image_array = image_to_array(file_path)
+        rows, cols = image_array.shape
+
+        np.savetxt("output.csv", image_array, delimiter=",", fmt="%d")
+
+        # Descomentar para medir tiempo
+        print('Working on: ', filename)
+        start_time = time.time()
+
+        mse_values.append([filename, run_c_program('output.csv', scales, rows, cols, m, r*calculate_std('output.csv'))])
+
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(execution_time)
+
+    return mse_values
+
+# Ejemplo:
+
+# '/home/bcm/Desktop/Repo/mse_c/datos/2D/500x500' => Hay que pasarle el path de la carpeta en donde estan las imagenes
+# 20 => escalas
+# 2 => m
+# 0.25 => r
+
+# Tambien imprime el tiempo en segundos que se demora.
+
+# En el ejemplo de abajo son imagenes chicas de 100x100, por eso hay que ponerle m=1 y r=0.5, pero con imagenes mas grandes la idea es ocupar m=2 y r=0.25
+
+# v = mse_2D('/home/bcm/Desktop/Repo/mse_c/datos/2D/100x100', 20, 1, 0.5)
+# plot_arrays(v)
