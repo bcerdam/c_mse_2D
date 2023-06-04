b23c999db2d3a3e8d1be39eb017feac7d67b0724
Adolfo Parra <adolfo.parra@uc.cl>
2023-06-04 19:59:44 -0400

    MSE 2D RGB

--- mse_2D.c
+++ mse_2D.c
@@ -250,3 +250,224 @@ int main(int argc, char *argv[]) {
 gcc mse_2D.c -o mse_2D -lm
 ./mse_2D
 */
+
+//  Para imágenes RGB
+
+double** rgb_read_csv(const char* filename1, const char* filename2, const char* filename3) {
+    double** data = NULL;
+    FILE* file1 = fopen(filename1, "r");
+    FILE* file2 = fopen(filename2, "r");
+    FILE* file3 = fopen(filename3, "r");
+    char line[1024];
+    int row_count = 0;
+    int col_count = 0;
+    
+    if (file1 == NULL || file2 == NULL || file3 == NULL) {
+        printf("Could not open one or more files.\n");
+        return NULL;
+    }
+    
+    // Count the number of rows and columns in the files
+    while (fgets(line, 1024, file1)) {
+        col_count = 0;
+        char* token = strtok(line, ",");
+        while (token != NULL) {
+            col_count++;
+            token = strtok(NULL, ",");
+        }
+        row_count++;
+    }
+    
+    // Allocate memory for the data array
+    data = (double**)malloc(3 * sizeof(double*));
+    for (int i = 0; i < 3; i++) {
+        data[i] = (double*)malloc(row_count * col_count * sizeof(double));
+    }
+    
+    // Reset the file pointers and read the data into the array
+    fseek(file1, 0, SEEK_SET);
+    fseek(file2, 0, SEEK_SET);
+    fseek(file3, 0, SEEK_SET);
+    int row = 0;
+    while (fgets(line, 1024, file1)) {
+        int col = 0;
+        char* token = strtok(line, ",");
+        while (token != NULL) {
+            data[0][row * col_count + col] = atof(token);
+            col++;
+            token = strtok(NULL, ",");
+        }
+        row++;
+    }
+    
+    row = 0;
+    while (fgets(line, 1024, file2)) {
+        int col = 0;
+        char* token = strtok(line, ",");
+        while (token != NULL) {
+            data[1][row * col_count + col] = atof(token);
+            col++;
+            token = strtok(NULL, ",");
+        }
+        row++;
+    }
+    
+    row = 0;
+    while (fgets(line, 1024, file3)) {
+        int col = 0;
+        char* token = strtok(line, ",");
+        while (token != NULL) {
+            data[2][row * col_count + col] = atof(token);
+            col++;
+            token = strtok(NULL, ",");
+        }
+        row++;
+    }
+    
+    fclose(file1);
+    fclose(file2);
+    fclose(file3);
+    return data;
+}
+
+
+float rgb_calculate_U_ij_m(double** image_r, double** image_g, double** image_b, int i, int j, int m, float* r, int H, int W, double delta, int fuzzy, int distance_type) {
+    float count = 0;
+    int N_m = (H - m) * (W - m);
+    for (int a = 0; a < H - m; a++) {
+        for (int b = 0; b < W - m; b++) {
+            float dist_r = max_distance(m, image_r, i, j, a, b, distance_type);
+            float dist_g = max_distance(m, image_g, i, j, a, b, distance_type);
+            float dist_b = max_distance(m, image_b, i, j, a, b, distance_type);
+            if (a == i && b == j) {
+                continue;
+            }
+            else {
+                if (fuzzy == 0) {
+                    if (dist_r <= r[0] && dist_g <= r[1] && dist_b <= r[2]) {
+                        count++;
+                    }
+                }
+                else if (fuzzy == 1) {
+                    count += fuzzy_membership(dist_r, r[0], delta) * fuzzy_membership(dist_g, r[1], delta) * fuzzy_membership(dist_b, r[2], delta);
+                }
+            }
+        }
+    }
+    return count / (N_m - 1);
+}
+
+float rgb_calculate_U_ij_m_plus_one(double** image_r, double** image_g, double** image_b, int i, int j, int m, float* r, int H, int W, double delta, int fuzzy, int distance_type) {
+    float count = 0;
+    int N_m = (H - m) * (W - m);
+    for (int a = 0; a < H - m; a++) {
+        for (int b = 0; b < W - m; b++) {
+            float dist_r = max_distance(m + 1, image_r, i, j, a, b, distance_type);
+            float dist_g = max_distance(m + 1, image_g, i, j, a, b, distance_type);
+            float dist_b = max_distance(m + 1, image_b, i, j, a, b, distance_type);
+            if (a == i && b == j) {
+                continue;
+            }
+            else {
+                if (fuzzy == 0) {
+                    if (dist_r <= r[0] && dist_g <= r[1] && dist_b <= r[2]) {
+                        count++;
+                    }
+                }
+                else if (fuzzy == 1) {
+                    count += fuzzy_membership(dist_r, r[0], delta) * fuzzy_membership(dist_g, r[1], delta) * fuzzy_membership(dist_b, r[2], delta);
+                }
+            }
+        }
+    }
+    return count / (N_m - 1);
+}
+
+float rgb_calculate_U_m(double** image_r, double** image_g, double** image_b, int m, float* r, int H, int W, double delta, int fuzzy, int distance_type) {
+    float sum = 0.0;
+    #pragma omp parallel for reduction(+:sum) num_threads(32)
+    for (int i = 0; i < H - m; i++) {
+        for (int j = 0; j < W - m; j++) {
+            sum += rgb_calculate_U_ij_m(image_r, image_g, image_b, i, j, m, r, H, W, delta, fuzzy, distance_type);
+        }
+    }
+    #pragma omp barrier
+    float average;
+    #pragma omp critical
+    {
+        average = sum / ((H - m) * (W - m));
+    }
+    return average;
+}
+
+float rgb_calculate_U_m_plus_one(double** image_r, double** image_g, double** image_b, int m, float* r, int H, int W, double delta, int fuzzy, int distance_type) {
+    float sum = 0.0;
+    #pragma omp parallel for reduction(+:sum) num_threads(32)
+    for (int i = 0; i < H - m; i++) {
+        for (int j = 0; j < W - m; j++) {
+            sum += rgb_calculate_U_ij_m_plus_one(image_r, image_g, image_b, i, j, m, r, H, W, delta, fuzzy, distance_type);
+        }
+    }
+    #pragma omp barrier
+    float average;
+    #pragma omp critical
+    {
+        average = sum / ((H - m) * (W - m));
+    }
+    return average;
+}
+
+float rgb_negative_logarithm(float um, float umplus1) {
+    if (um == 0) {
+        return 0;
+    }
+    else {  
+        float result = -log(umplus1 / um);
+        return result;  
+    }
+}
+
+// int main(int argc, char* argv[]) {
+//     // if (argc < 14) {
+//     //     printf("Usage: %s csv_path1 csv_path2 csv_path3 scales rows cols m r1 r2 r3 delta fuzzy distance_type\n", argv[0]);
+//     //     return 1;
+//     // }
+
+//     const char* csv_path1 = argv[1];
+//     const char* csv_path2 = argv[2];
+//     const char* csv_path3 = argv[3];
+
+//     double** data = rgb_read_csv(csv_path1, csv_path2, csv_path3);
+
+//     int scales = atoi(argv[4]);
+//     int rows = atoi(argv[5]);
+//     int cols = atoi(argv[6]);
+//     int m = atoi(argv[7]);
+
+//     float r[3] = {atof(argv[8]), atof(argv[9]), atof(argv[10])};
+//     double delta = atof(argv[11]);
+//     int fuzzy = atoi(argv[12]);
+//     int distance_type = atoi(argv[13]);
+
+//     double* n_values = malloc(scales * sizeof(double));
+
+//     for (int i = 1; i <= scales; i++) {
+//         double** coarse_data_r = coarse_graining(data, rows, cols, i);
+//         double** coarse_data_g = coarse_graining(data, rows, cols, i);
+//         double** coarse_data_b = coarse_graining(data, rows, cols, i);
+
+//         double U_m = rgb_calculate_U_m(coarse_data_r, coarse_data_g, coarse_data_b, m, r, rows / i, cols / i, delta, fuzzy, distance_type);
+//         double U_m_plus_one = rgb_calculate_U_m_plus_one(coarse_data_r, coarse_data_g, coarse_data_b, m, r, rows / i, cols / i, delta, fuzzy, distance_type);
+//         double n = rgb_negative_logarithm(U_m, U_m_plus_one);
+//         n_values[i - 1] = n;
+//     }
+
+//     for (int i = 0; i < scales; i++) {
+//         printf("%f ", n_values[i]);
+//     }
+//     printf("\n");
+
+//     free(n_values);
+
+//     return 0;
+// }
--- mse_2D_C.py
+++ mse_2D_C.py
@@ -54,7 +54,7 @@ def run_c_program(csv_path, scales, rows, cols, m, r, delta, fuzzy, distance_typ
     n_values = [np.inf if x == '1.#INF00' else float(x) for x in n_values]
     return n_values
 
-def mse_2D(folder_path, scales, m, r, delta, fuzzy=False, distance_type=0):
+def mse_2D(folder_path, scales, m, r, delta, fuzzy, distance_type):
     if fuzzy == False:
         fuzzy = 0
     elif fuzzy == True:
@@ -79,13 +79,18 @@ def mse_2D(folder_path, scales, m, r, delta, fuzzy=False, distance_type=0):
 
     return mse_values
 
-def mse_2D_one_image(image_path, scales, m, r):
+def mse_2D_one_image(image_path, scales, m, r, delta, fuzzy, distance_type):
+    if fuzzy == False:
+        fuzzy = 0
+    elif fuzzy == True:
+        fuzzy = 1
+
     image_array = image_to_array(image_path)
     rows, cols = image_array.shape
 
     np.savetxt("output.csv", image_array, delimiter=",", fmt="%d")
 
-    return run_c_program('output.csv', scales, rows, cols, m, r*calculate_std('output.csv'))
+    return run_c_program('output.csv', scales, rows, cols, m, r*calculate_std('output.csv'), delta, fuzzy, distance_type)
 
 # Ejemplo:
 
@@ -103,3 +108,102 @@ def mse_2D_one_image(image_path, scales, m, r):
 
 # v = mse_2D('/home/bcm/Desktop/Repo/c_mse_2D/datos/2D/100x100', 20, 1, 0.5, 0.8, fuzzy=False)
 # plot_arrays(v)
+
+# Para imágenes en RGB
+
+def rgb_image_to_array(image_path):
+    img = Image.open(image_path).convert('RGB')
+    red_band, green_band, blue_band = img.split()
+    red_band = np.array(red_band)
+    green_band = np.array(green_band)
+    blue_band = np.array(blue_band)
+    
+    return (red_band, green_band, blue_band)
+
+# def mse_2D(folder_path, scales, m, r, delta, fuzzy=False, distance_type=0):
+#     if fuzzy == False:
+#         fuzzy = 0
+#     elif fuzzy == True:
+#         fuzzy = 1
+#     mse_values = []
+#     for filename in os.listdir(folder_path):
+#         file_path = os.path.join(folder_path, filename)
+#         red_band, green_band, blue_band = rgb_image_to_array(file_path)
+#         rows, cols = red_band.shape
+
+
+#         # np.savetxt("output.csv", image_array, delimiter=",", fmt="%d")
+
+#         # Descomentar para medir tiempo
+#         print('Working on: ', filename)
+#         start_time = time.time()
+
+#         mse_values.append([filename, run_c_program('output.csv', scales, rows, cols, m, r*calculate_std('output.csv'), delta, fuzzy, distance_type)])
+
+#         end_time = time.time()
+#         execution_time = end_time - start_time
+#         print(execution_time)
+
+#     return mse_values
+
+def rgb_mse_2D_one_image(image_path, scales, m, r, delta, fuzzy, distance_type):
+    if fuzzy == False:
+        fuzzy = 0
+    elif fuzzy == True:
+        fuzzy = 1
+
+    red_band, green_band, blue_band = rgb_image_to_array(image_path)
+    rows, cols = red_band.shape
+
+    np.savetxt("red_band.csv", red_band, delimiter=",", fmt="%d")
+    np.savetxt("green_band.csv", green_band, delimiter=",", fmt="%d")
+    np.savetxt("blue_band.csv", blue_band, delimiter=",", fmt="%d")
+    files = ['red_band.csv', 'green_band.csv', 'blue_band.csv']
+
+    # vector_r = np.array([r*calculate_std('red_band.csv'), r*calculate_std('green_band.csv'), r*calculate_std('blue_band.csv')])
+    vector_r = [r*calculate_std('red_band.csv'), r*calculate_std('green_band.csv'), r*calculate_std('blue_band.csv')]
+
+    return rgb_run_c_program(files, scales, rows, cols, m, vector_r, delta, fuzzy, distance_type)
+
+def rgb_run_c_program(csv_paths, scales, rows, cols, m, vector_r, delta, fuzzy, distance_type):
+    r_1 = vector_r[0]
+    r_2 = vector_r[1]
+    r_3 = vector_r[2]
+    # command = ['./mse_2D'] + csv_paths + [str(scales), str(rows), str(cols), str(m), str(vector_r), str(delta), str(fuzzy), str(distance_type)]
+    command = ['mse_2D'] + csv_paths + [str(scales), str(rows), str(cols), str(m), str(r_1), str(r_2), str(r_3), str(delta), str(fuzzy), str(distance_type)]
+    print(command)
+
+    result = subprocess.run(command, stdout=subprocess.PIPE)
+    print(result.stdout)
+    output = result.stdout.decode('utf-8').strip()
+    n_values = list(output.split())
+    # n_values = [float(x) for x in n_values]
+    n_values = [np.inf if x == '1.#INF00' else float(x) for x in n_values]
+    return n_values
+
+# Ejemplo:
+
+# image_path = r'datos\2D\imagenes_prueba\atardecer.jpeg'
+# scales = 12
+# m = 2
+# r = 0.25
+# delta = 0.8
+# fuzzy = False
+# distance_type = 0
+
+# v = rgb_mse_2D_one_image(image_path, scales, m, r, delta, fuzzy, distance_type)
+# print(v)
+
+image_path = r'datos\2D\imagenes_redimensionadas\atardecer.jpeg'
+scales = 12
+m = 2
+r = 0.25
+delta = 0.8
+fuzzy = False
+distance_type = 0
+
+mse_values = mse_2D_one_image(image_path, scales, m, r, delta, fuzzy, distance_type)
+print(mse_values)
+
+
+
